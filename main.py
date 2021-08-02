b9dcf73042f28086e1ec4cc5120d5c209b5d4bb0
mohak chandna <68562942+Mohak634@users.noreply.github.com>
2021-08-02 06:01:45 +0000

    bot is working 2-8-21 11:31

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import discord
+import gspread
 from discord import partial_emoji
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
 from discord.partial_emoji import PartialEmoji
 
-client = discord.Client()
 
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("cred.json", scope)
+# x = message.guild.members
+# print(x)
+
+# client = discord.Client()
+# from discord.ext import commands
+
+# bot = commands.Bot(command_prefix='!')
+
+# @bot.command(pass_context=True)
+# async def poke(ctx, member: discord.Member):
+#     await bot.send_message(member, 'boop')
+    
+# bot.run('ODY5NTMyNjYxNjA0MDM2NjQ5.YP_lZQ.r7VEAReL1HIT0Zo106pUPaxJN8E')
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -32,6 +50,7 @@ class MyClient(discord.Client):
             return
 
         try:
+
             role_id = self.emoji_to_role[payload.emoji]
         except KeyError:
             print('hello')
@@ -52,6 +71,21 @@ class MyClient(discord.Client):
         except discord.HTTPException:
             # If we want to do something in case of errors we'd do it here.
             pass
+        if payload.channel_id == 869519092338528286:
+
+          channel = await client.fetch_channel(payload.channel_id)
+          message = await channel.fetch_message(self.role_message_id)
+
+          # iterating through each reaction in the message
+          # print(message.reactions)
+          for r in message.reactions:
+
+              # checks the reactant isn't a bot and the emoji isn't the one they just reacted with
+              # print(payload.emoji, r)
+              if payload.member in await r.users().flatten() and  not payload.member.bot and str(r) != str(payload.emoji):
+
+                  # removes the reaction
+                  await message.remove_reaction(r.emoji, payload.member)
 
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         """Removes a role based on a reaction emoji."""
@@ -88,6 +122,7 @@ class MyClient(discord.Client):
         except discord.HTTPException:
             # If we want to do something in case of errors we'd do it here.
             pass
+        
 
 
 
@@ -115,6 +150,50 @@ class MyClient(discord.Client):
             await message.channel.send('gamer')
         elif message.content == "!help":
             await message.channel.send("!hello: hi\n!users: No of participants in server\n!mohak: gamer")
+        elif message.content.startswith('!mute') and message.author.guild_permissions.administrator:
+            # await message.mentions[0].remove_roles(message.mentions[0].roles)
+            await message.mentions[0].add_roles(discord.utils.get(message.guild.roles, name="muted"))
+            await message.channel.send('ok')
+        elif message.content.startswith('!unmute') and message.author.guild_permissions.administrator:
+            # await message.mentions[0].remove_roles(message.mentions[0].roles)
+            await message.mentions[0].remove_roles(discord.utils.get(message.guild.roles, name="muted"))
+            await message.channel.send('ok')
+        elif message.content == "!edit":
+          pass
+
+
+
+        elif message.content.startswith('!result'):
+          name_of_sheet = message.content.split()[1].lower()
+          sheet = gspread.authorize(creds).open("Results").worksheet(name_of_sheet)
+          data = sheet.get_all_records()
+          embedVar = discord.Embed(title="Results", description= '')
+          for i in data:
+              embedVar.add_field(name=f"{i['Position']}" , value=f"Name: {i['Name']}\nSchool: {i['School']}", inline=False)
+        
+            
+          # embedVar.add_field(name="Create[X]", value="1st: Baibhav\n2nd: Baibhav\n3rd: Baibhav", inline=False)
+          
+          # embedVar.add_field(name="Field2", value="hi2", inline=False)
+          await message.channel.send(embed=embedVar)
+
+        elif message.content.startswith('!member'):
+          for guild in client.guilds:
+            for member in guild.members:
+              # me = await client.get_user_info('MY_SNOWFLAKE_ID')
+              # await client.send_message(me, "Hello!")
+              await client.send_message(member, 'boop')
+                  # print(type(member))
+                  # member.send('hi')
+                  # channel = await member.create_dm()
+                  # user=await client.get_user_info(428220061757472768)
+                  # await client.send_message(user, "Your message goes here")
+                  # pass
+                  # await channel.send('message')
+                  # await member.send('t')
+                # await member.send('t')
+                # print(member.display_name.split('|')[0])
+                
         elif message.content == "!get-roles":
             await message.channel.send('''React with the corresponding emoji to get the role for your event 
 Create[X] - :art:
@@ -131,8 +210,13 @@ ComicDes - <:paintbrush2:869862753748258847>
         guild = member.guild
         if guild.system_channel is not None:
             to_send = f'Welcome {member.mention} to {guild.name}!'
-            await guild.get_channel(847402192587718660).send(to_send)
-
+            await guild.get_channel(869671953500356649).send(to_send)
+      # @client.command(pass_context=True)
+      # async def dm(ctx):
+      #     user=await client.get_user_info("User's ID here")
+      #     await client.send_message(user, "Your message goes here")
+      
+      
 
 
 #     def __init__(self, *args, **kwargs):
